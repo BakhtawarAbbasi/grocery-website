002beace3a4ecae5e98e4fb5c7c7e78ad2ca531e
BakhtawarAbbasi <bakhtawarabbasi009@gmail.com>
2025-08-03 17:23:30 +0500

    .

--- app/api/products/[id]/route.ts
+++ app/api/products/[id]/route.ts
@@ -1,18 +1,15 @@
-// app/api/products/[id]/route.ts
 import { NextResponse } from "next/server";
 
 export async function GET(
   request: Request,
-  context: { params: { id: string } }
+  context: { params: Promise<{ id: string }> }
 ) {
-  const { id } = context.params; // ✅ yahan se params destructure kiya
+  const { id } = await context.params;
 
   try {
     const res = await fetch(
       `https://api-for-ecommerce-website.onrender.com/products/${id}`,
-      {
-        cache: "no-store",
-      }
+      { cache: "no-store" }
     );
 
     if (!res.ok) {
@@ -20,7 +17,6 @@ export async function GET(
     }
 
     const data = await res.json();
-
     const fixedData = {
       ...data,
       image: data.image.startsWith("http")
